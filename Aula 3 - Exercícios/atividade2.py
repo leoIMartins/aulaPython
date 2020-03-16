2c27eabaeb25c689f0777d375b633b1d359450d7
Leonardo Martins <lmartins8@dxc.com>
2020-03-16 16:10:06 -0300

    Exercicio 2

---
+++ Aula 3 - Exercícios/atividade2.py
@@ -0,0 +1,35 @@
+arq = open('C:\\Users\\lmartins8\\projetos\\aulapython01\\Aula 3 - Exercícios\\carros.txt', 'w')
+carros = []
+carros.append('Gol\n')
+carros.append('Uno\n')
+carros.append('Palio\n')
+carros.append('EcoSport\n')
+carros.append('Clio\n')
+carros.append('Strada\n')
+carros.append('Golf\n')
+arq.writelines(carros)
+arq.close()
+
+'''arq2 = open('C:\\Users\\lmartins8\\projetos\\aulapython01\\Aula 3 - Exercícios\\carros em ordem inversa.txt', 'w')
+carrosOrdemInversa = sorted(carros, reverse=True)
+arq2.writelines(carrosOrdemInversa)
+arq.close'''
+
+arq2 = open('C:\\Users\\lmartins8\\projetos\\aulapython01\\Aula 3 - Exercícios\\carros em ordem inversa.txt', 'w')
+carrosOrdemInversa = carros[::-1]
+arq2.writelines(carrosOrdemInversa)
+arq.close
+
+arq3 = open('C:\\Users\\lmartins8\\projetos\\aulapython01\\Aula 3 - Exercícios\\carros em ordem alfabetica.txt', 'w')
+carrosOrdemAlfabetica = sorted(carros)
+arq3.writelines(carrosOrdemAlfabetica)
+arq.close
+
+arq4 = open('C:\\Users\\lmartins8\\projetos\\aulapython01\\Aula 3 - Exercícios\\carros numerados.txt', 'w')
+i = 1
+carrosNumerados = []
+for cars in carrosOrdemAlfabetica:
+    carrosNumerados.append("%d - %s" % (i, cars))
+    i += 1
+arq4.writelines(carrosNumerados)
+arq.close
