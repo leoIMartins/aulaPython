d491542991e7b6a33a31a502fb241c9eb901a6d7
Leonardo Ignacio Martins <leonardo_ignacio2010@hotmail.com>
2020-04-01 02:33:37 -0300

    Modificações aula 4

---
+++ Aula 4/Aplicação com menu/Filmes.py
@@ -0,0 +1,70 @@
+filmes = []
+avaliacoes= []
+
+while True:
+    opcao = input("\n1 - Inserir Filme  || "
+                  "2 - Listar Filmes || "
+                  "3 - Excluir Filme || "
+                  "4 - Inserir Avaliação || "
+                  "5 - Listar Avaliações\n"
+                  "6 - Associar Avaliação ao Filme || "
+                  "7 - Imprimir Relatório (Filme/Avaliação) || "
+                  "8 - Sair\n"
+                  "Opção escolhida: ")
+
+    if opcao == "1":
+        filme = input("Informe o nome do filme: ")
+        filmes.append(filme)
+        print("O filme %s foi adicionado à lista!" % filme)
+
+    elif opcao == "2":
+        print("Filmes inseridos:")
+        i = 1
+        for f in filmes:
+            print("\t\t%d - %s" % (i, f))
+            i += 1
+
+    elif opcao == "3":
+        excluir = input("Informe o nome do filme a ser excluído: ")
+        if filmes.__contains__(excluir):
+            filmes.remove(excluir)
+            print("O filme %s foi removido da lista!" % excluir)
+        else:
+            print("O filme %s não existe na lista!" % excluir)
+
+    elif opcao == "4":
+        avaliacao = input("Descreva a avaliação: ")
+        avaliacoes.append(avaliacao)
+        print("A avaliação foi adicionada com sucesso!")
+
+    elif opcao == "5":
+        print("Avaliações inseridas:")
+        i = 1
+        for a in avaliacoes:
+            print("\t\t%d - %s" % (i, a))
+            i += 1
+
+    elif opcao == "6":
+        i = 1
+        for f in filmes:
+            print("\t\t%d - %s" % (i, f))
+            i += 1
+        opcaoFilme = int(input("Informe o número do filme: ")) - 1
+
+        i = 1
+        for a in avaliacoes:
+            print("\t\t%d - %s" % (i, a))
+            i += 1
+        opcaoAvaliacao = int(input("Informe o número da avaliação a ser associada ao filme: ")) - 1
+
+        print("A avaliação %s foi associada ao filme %s!" % (avaliacoes[opcaoAvaliacao], filmes[opcaoFilme]))
+
+    elif opcao == "7":
+        rel = open('C:\\Python\\Aula 4\\Aplicação com menu\\Relatorio.txt', 'w')
+
+
+    elif opcao == "8":
+        break
+
+    else:
+        print("\nOpção inválida! Informe uma das seguintes opções:")
