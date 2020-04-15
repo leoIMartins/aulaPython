6c1c273143dfc1d3e314a9f36e1c7c7d241c4287
Leonardo Ignacio Martins <leonardo_ignacio2010@hotmail.com>
2020-04-15 00:52:23 -0300

    mega sena

--- Aula 6/MegaSena.py
+++ Aula 6/MegaSena.py
@@ -1,6 +1,32 @@
-numeros = []
+import random
 
-for i in range(6):
-    while numeros[i] < 1 or numeros[i] > 60:
-        numeros.append(input("Digite o %d número: " % (i)))
-print(numeros)
+numeros = []
+numerosSorteio = []
+
+def escolherNumeros():
+    i = 1
+    while len(numeros) < 6:
+        numero = int(input("\nDigite o %dº número: " % i))
+        if numeros.__contains__(numero):
+            return print("Os números não podem se repetir")
+        elif numero < 1 or numero > 60:
+            return print("Informe apenas números entre 1 e 60")
+        else:
+            numeros.append(numero)
+            i += 1
+
+
+def gerarJogo():
+    while len(numerosSorteio) < 6:
+        sorteio = random.randint(1, 60)
+        if numerosSorteio.__contains__(sorteio):
+            break
+        else:
+            numerosSorteio.append(sorteio)
+    return numerosSorteio
+
+escolherNumeros()
+sorteioMegaSena = gerarJogo()
+
+print("\nOs números escolhidos foram: " + str(numeros))
+print("Os números sorteados foram: " + str(sorteioMegaSena))
