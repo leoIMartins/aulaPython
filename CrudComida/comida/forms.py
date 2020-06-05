a49ba66b63ef768ca834a6b7019d1f003b34004d
Leonardo Ignacio Martins <leonardo_ignacio2010@hotmail.com>
2020-06-04 22:29:35 -0300

    tst

---
+++ CrudComida/comida/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from comida.models import Comida
+
+
+class ComidaForm(forms.ModelForm):
+    class Meta:
+        model = Comida
+        fields = ['tipoComida', 'descricao', 'quantidade', 'opcoes', 'valorCalorico', 'salada']
